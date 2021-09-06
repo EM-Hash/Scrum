7238d214f9ac7494fd187ffc703756eb28246038
SHXBOT <ehinners@my.wctc.edu>
2021-09-06 08:39:56 -0500

    Data Entry logic

--- Program.cs
+++ Program.cs
@@ -32,7 +32,25 @@ namespace ehinnersSleepData
                 
                 // random number generator
                 Random rnd = new Random();
+                
                 StreamWriter sw = new StreamWriter("data.txt");
+
+                // loop for the desired # of weeks
+                while (dataDate < dataEndDate)
+                {
+                    // 7 days in a week
+                    int[] hours = new int[7];
+                    for (int i = 0; i < hours.Length; i++)
+                    {
+                        // generate random number of hours slept between 4-12 (inclusive)
+                        hours[i] = rnd.Next(4, 13);
+                    }
+                    // M/d/yyyy,#|#|#|#|#|#|#
+                    //Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+                    sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
+                    // add 1 week to date
+                    dataDate = dataDate.AddDays(7);
+                }
 
                 sw.Close();
 
