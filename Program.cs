06a6c0ca70fa5b8ba7e60aa20816003ea1043fa1
SHXBOT <ehinners@my.wctc.edu>
2021-09-06 08:23:36 -0500

    Initial Commit (User Input)

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ehinnersSleepData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ask for input
+            Console.WriteLine("Enter 1 to create data file.");
+            Console.WriteLine("Enter 2 to parse data.");
+            Console.WriteLine("Enter anything else to quit.");
+            // input response
+            string resp = Console.ReadLine();
+
+            if (resp == "1")
+            {
+                
+            }
+            else if(resp=="2")
+            {
+
+            }
+                
+        }
+    }
+}
